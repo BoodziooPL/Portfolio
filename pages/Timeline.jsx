77775e4dd62f9eba83d34469be32d8b1004359f2
BoodziooPL <bogdanmalik@gmail.com>
2022-11-29 23:25:16 +0100

    timeline section

--- pages/Timeline.jsx
+++ pages/Timeline.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
-import RedPin from '../assets/pinred.png'
-import YellowPin from '../assets/pinyellow.png'
-import { ImArrowRight } from 'react-icons/im'
-import FirstInfo from '../components/FirstInfo'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 const handlerClick = () => {
@@ -12,21 +7,95 @@ const handlerClick = () => {
 }
 export default function Timeline() {
   return (
-    <div className='bg-[#464082] min-h-screen'>
-      <div className='w-full h-full'>
-        <h1>TimeLine:</h1>
-        <div className='insert'></div>
-        <div className='bg-gray-400 h-[1px] flex items-center justify-around mt-96'>
-          <p>2004</p>
-          <div
-            className='bg-gray-300 h-2 w-2 rounded-full '
-            onClick={handlerClick}></div>
-          <p>2004</p>
-          <div className='bg-gray-300 h-2 w-2 rounded-full'></div>
-          <p>2004</p>
-          <div className='bg-gray-300 h-2 w-2 rounded-full'></div>
-          <p>2004</p>
-          <div className='bg-gray-300 h-2 w-2 rounded-full'></div>
+    <div className='bg-[#464082] min-h-screen w-full flex justify-center items-center'>
+      <div class='max-w-7xl mx-auto w-full grid grid-cols-9 px-2'>
+        <div class='col-span-4 w-full h-full '>
+          <div class='w-full h-full bg-indigo-400 rounded-md p-2 md:pl-4'>
+            <h1 class='text-white text-xl font-medium py-2'>2004</h1>
+            <p class='text-gray-100 sm:text-sm text-xs'>First PC</p>
+          </div>
+        </div>
+        <div class='relative col-span-1 w-full h-full flex justify-center items-center'>
+          <div class='h-full w-1 bg-indigo-300'></div>
+          <div class='absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center'>
+            1
+          </div>
+        </div>
+        <div class='col-span-4 w-full h-full'></div>
+
+        <div class='col-span-4 w-full h-full'></div>
+        <div class='relative col-span-1 w-full h-full flex justify-center items-center'>
+          <div class='h-full w-1 bg-indigo-300'></div>
+          <div class='absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center'>
+            2
+          </div>
+        </div>
+        <div class='col-span-4 w-full h-full '>
+          <div class='w-full h-full bg-indigo-400 rounded-md p-2 md:pl-4'>
+            <h1 class='text-white text-xl font-medium py-2'>2010</h1>
+            <p class='text-gray-100 sm:text-sm text-xs'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
+              quas omnis vero totam ullam, reprehenderit ratione pariatur
+              accusamus suscipit odit nostrum?
+            </p>
+          </div>
+        </div>
+
+        <div class='col-span-4 w-full h-full '>
+          <div class='w-full h-full bg-indigo-400 rounded-md p-2 md:pl-4'>
+            <h1 class='text-white text-xl font-medium py-2'>2014</h1>
+            <p class='text-gray-100 sm:text-sm text-xs'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
+              quas omnis vero totam ullam, reprehenderit ratione pariatur
+              accusamus suscipit odit nostrum?
+            </p>
+          </div>
+        </div>
+        <div class='relative col-span-1 w-full h-full flex justify-center items-center'>
+          <div class='h-full w-1 bg-indigo-300'></div>
+          <div class='absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center'>
+            3
+          </div>
+        </div>
+        <div class='col-span-4 w-full h-full'></div>
+
+        <div class='col-span-4 w-full h-full'></div>
+        <div class='relative col-span-1 w-full h-full flex justify-center items-center'>
+          <div class='h-full w-1 bg-indigo-300'></div>
+          <div class='absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center'>
+            4
+          </div>
+        </div>
+        <div class='col-span-4 w-full h-full '>
+          <div class='w-full h-full bg-indigo-400 rounded-md p-2 md:pl-4'>
+            <h1 class='text-white text-xl font-medium py-2'>2020</h1>
+            <p class='text-gray-100 sm:text-sm text-xs'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
+              quas omnis vero totam ullam, reprehenderit ratione pariatur
+              accusamus suscipit odit nostrum?
+            </p>
+          </div>
+        </div>
+
+        <div class='col-span-4 w-full h-full '>
+          <div class='w-full h-full bg-indigo-400 rounded-md p-2 md:pl-4'>
+            <h1 class='text-white text-xl font-medium py-2'>Futured</h1>
+            <p class='text-gray-100 sm:text-sm text-xs'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              corporis consequuntur voluptate nulla iusto quam ut quasi, eaque
+              quas omnis vero totam ullam, reprehenderit ratione pariatur
+              accusamus suscipit odit nostrum?
+            </p>
+          </div>
+        </div>
+        <div class='relative col-span-1 w-full h-full flex justify-center items-center'>
+          <div class='h-full w-1 bg-indigo-300'></div>
+          <div class='absolute w-6 h-6 rounded-full bg-indigo-400 z-10 text-white text-center'>
+            5
+          </div>
         </div>
       </div>
 
