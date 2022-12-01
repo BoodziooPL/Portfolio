6edc98a899983f120c8787a3ea03c2d0a28e567f
BoodziooPL <bogdanmalik@gmail.com>
2022-12-01 22:25:51 +0100

    commit

--- pages/Timeline.jsx
+++ pages/Timeline.jsx
@@ -80,8 +80,7 @@ export default function Timeline() {
           <div class='w-full h-full bg-indigo-400 rounded-md p-2 md:pl-4'>
             <h1 class='text-white text-xl font-medium py-2'>2020</h1>
             <p class='text-gray-100 sm:text-sm text-xs'>
-              I Started learning on Udemy.com with courses
-              <quote> samuraj programowania</quote>
+              I Started learning on Udemy.com with courses samuraj programowania
               and others sources on youtube. I focused on getting more knowledge
               Javascript, CSS and RWD.
             </p>
